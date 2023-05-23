39103d4a3aa680e74497354778068e70acc5409f
Afsan Saeed <afsansaeed2017@gmail.com>
2023-05-23 08:24:50 +0600

    API healthcheck added

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HealthCheckView(APIView):
+    def get(self, request):
+        return Response({
+            "message": "Health check passed",
+            "status": 200
+        })
 
 urlpatterns = [
+    path('', HealthCheckView.as_view(), name='health_check'),
     path('admin/', admin.site.urls),
     path('api/', include('crm.urls'), name='api'),
     path('user/', include('user.urls'), name='user'),
 ]
+
